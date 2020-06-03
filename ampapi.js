04ab927373b5dbcd505e1d854b7a64e5cba7d8c5
Michael Biggins <mike@cubecoders.com>
2020-06-03 22:21:47 +0100

    Remove lots of browser-type fluff and slim down API considerably.

--- ampapi.js
+++ ampapi.js
@@ -1,29 +1,24 @@
 exports.AMPAPI = function (baseUri) {
     const request = require('request');
     var self = this;
-	this.baseUri = baseUri;
-	this.sessionId = "";
-	this.dataSource = "";
-	this.API = { Core: { GetAPISpec: [] } };
-	this.initAsync = async function (stage2) {
-		this.dataSource = 
-			(baseUri.endsWith("/") ? 
-				baseUri.substring(0, baseUri.lastIndexOf("/")) : 
-				baseUri)
-			+ "/API";
-			
-		for (const module of Object.keys(this.API)) {
+    this.baseUri = baseUri;
+    this.sessionId = "";
+    this.dataSource = "";
+    this.API = { Core: { GetAPISpec: [] } };
+    this.initAsync = async function (stage2) {
+        this.dataSource = (baseUri.endsWith("/") ?
+                baseUri.substring(0, baseUri.lastIndexOf("/")) :
+                baseUri)
+            + "/API";
+
+        for (const module of Object.keys(this.API)) {
             var methods = this.API[module];
             this[module] = {};
 
             for (const method of Object.keys(methods)) {
                 this[module][method + "Async"] = function (params) {
                     var args = Array.prototype.slice.call(arguments, 0);
-                    return new Promise(function (resolve, reject) {
-                        args.push(resolve);
-                        args.push(reject);
-                        self.APICall(module, method, args);
-                    });
+                    return self.APICall(module, method, args);
                 };
             }
         }
@@ -41,8 +36,8 @@ exports.AMPAPI = function (baseUri) {
                 return false;
             }
         }
-	}
-	this.APICall = function(module, methodName, args) {
+    }
+    this.APICall = function (module, methodName, args) {
         var data = {};
 
         var methodParams = this.API[module][methodName].Parameters;
@@ -56,40 +51,29 @@ exports.AMPAPI = function (baseUri) {
             data[argName] = val;
         }
 
-        var callback = null;
-        var failCallback = null;
-
-        if (args.length == (methodParamsLength + 1)) {
-            callback = args[args.length - 1];
-        }
-        else if (args.length == (methodParamsLength + 2)) {
-            callback = args[args.length - 2];
-            failCallback = args[args.length - 1];
-        }
-
-        var URI = this.dataSource + "/" + module + "/" + methodName;
+        var URI = `${this.dataSource}/${module}/${methodName}`;
         data.SESSIONID = this.sessionId;
-        request.post({
-			headers: {"Accept": "application/vnd.cubecoders-ampapi", "User-Agent": "CCL/AMPAPI-NodeJS"},
-			url: URI,
-			body: JSON.stringify(data),
-		}, function (error, res, body) {
-			if (error && failCallback)
-			{
-				failCallback(error, res);
-			}
-			else if (!error)
-			{
-                var result = JSON.parse(body);
 
-                if (result != null && Object.keys(result).length === 1 && result.result != undefined) {
-                    callback(result.result);
+        return new Promise(function (resolve, reject) {
+            request.post({
+                headers: { "Accept": "application/vnd.cubecoders-ampapi", "User-Agent": "CCL/AMPAPI-NodeJS" },
+                url: URI,
+                body: JSON.stringify(data),
+            }, function (error, res, body) {
+                if (error) {
+                    reject(error, res);
                 }
-                else
-                {
-                    callback(result);
+                else if (!error) {
+                    var result = JSON.parse(body);
+
+                    if (result != null && Object.keys(result).length === 1 && result.result != undefined) {
+                        resolve(result.result);
+                    }
+                    else {
+                        resolve(result);
+                    }
                 }
-			}
-		});
+            });
+        });
     }
 };
